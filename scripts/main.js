694639d39e44c7c0d170f81a903e20dd7adcdba7
streamedLine <dovidlwtn@gmail.com>
2018-07-26 02:49:30 -0400

    done

--- scripts/main.js
+++ scripts/main.js
@@ -17,55 +17,56 @@
 		initButtons();
 	}());
 
-	// //Show More buttons
-	// (function() {
-	// 	const buttons = Array.from(document.getElementsByClassName('show-more-btn'));
-	// 	const videosDivArray = Array.from(document.getElementsByClassName('videos'));
-	// 	const map = {
-	// 		'mobile-invisible': 1,
-	// 		'small-wide-invisible': 3,
-	// 		'wide-invisible': 4
-	// 	};
+	//Show More buttons
+	(function() {
+		const buttons = Array.from(document.getElementsByClassName('show-more-btn'));
+		const videosDivArray = Array.from(document.getElementsByClassName('videos'));
+		const map = {
+			'mobile-invisible': 1,
+			'small-wide-invisible': 3,
+			'wide-invisible': 4
+		};
 
-	// 	const initButtons = () => {
-	// 		buttons.forEach(bindIndexToEvent)
-	// 	}
+		const initButtons = () => {
+			buttons.forEach(bindIndexToEvent)
+		}
 
-	// 	function bindIndexToEvent(button, i) {
-	// 		button.addEventListener('click', function(){
-	// 			let size = document.getElementsByClassName('fuel-your-passion-img')[0].offsetWidth;
+		function bindIndexToEvent(button, i) {
+			button.addEventListener('click', function(){
+				let size = document.getElementsByClassName('fuel-your-passion-img')[0].offsetWidth;
 
-	// 			if (size <= 320) {
-	// 				size = 'mobile-invisible';
-	// 			}	else if (size <= 960) {
-	// 				size = 'small-wide-invisible';
-	// 			}	else {
-	// 				size = 'wide-invisible';
-	// 			}
-	// 			console.log(this)
-	// 			this.getElementsByClassName('Show-More')[0].innerText = 'LOADING';
+				if (size <= 320) {
+					size = 'mobile-invisible';
+				}	else if (size <= 960) {
+					size = 'small-wide-invisible';
+				}	else {
+					size = 'wide-invisible';
+				}
+				console.log(size)
+				this.getElementsByClassName('Show-More')[0].innerText = 'LOADING';
 
-	// 			let videos = Array.from(videosDivArray[i].getElementsByClassName('video'))
-	// 			videos = videos.filter(video => video.classList.contains(size));
-	// 			videos = videos.slice(0, map[size]);
+				let videos = Array.from(videosDivArray[i].getElementsByClassName('video'))
+				videos = videos.filter(video => video.classList.contains(size));
+				videos = videos.slice(0, map[size]);
 				
-	// 			console.log(videos)
+				console.log(videos)
 
-	// 			videos.forEach((video) => {		
-	// 				let extraHeight = map[size] == 1 ? 0 : 20;
+				videos.forEach((video) => {		
+					let extraHeight = map[size] == 1 ? 10 : 60;
 
-	// 				videosDivArray[i].style.height = (videosDivArray[i].clientHeight + 1) +'px';
-	// 				videosDivArray[i].style.height = (videosDivArray[i].clientHeight + 249) +'px';
-	// 				video.classList.toggle(size)
-	// 			});
+					videosDivArray[i].style.maxHeight = (videosDivArray[i].clientHeight + 1) +'px';
+					videosDivArray[i].style.maxHeight = (videosDivArray[i].clientHeight + extraHeight + 249) +'px';
+					console.log(video)
+					video.classList.toggle(size)
+				});
 
-	// 			this.getElementsByClassName('Show-More')[0].innerText = 'Show More';
-	// 		});
-	// 	}
+				this.getElementsByClassName('Show-More')[0].innerText = 'Show More';
+			});
+		}
 
 		
-	// 	initButtons();
-	// }());
+		initButtons();
+	}());
 
 
 	
